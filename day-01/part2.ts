ace40d1f8a6a6b45701cc69243471dc35c1db83e
mattbalmer <signup@mattbalmer.com>
2021-12-03 00:30:38 -0800

    Avoid summing each iteration on day1

--- day-01/part2.ts
+++ day-01/part2.ts
@@ -2,14 +2,16 @@ const sum = (list: number[]): number => list.reduce((sum, e) => sum + e, 0);
 
 export const countWindowIncreases = (inputs: number[], windowSize: number): number => {
   let increases = 0;
-  let previousSum;
+  let previousSum = sum(inputs.slice(0, windowSize));
 
   if (inputs.length < windowSize + 1) {
     return 0;
   }
 
-  for(let i = 0; i <= inputs.length - windowSize; i++) {
-    let currentSum = sum(inputs.slice(i, i + windowSize));
+  for(let i = 1; i <= inputs.length - windowSize; i++) {
+    const previousValue = inputs[i - 1];
+    const newValue = inputs[i + windowSize - 1];
+    let currentSum = previousSum - previousValue + newValue;
     if (previousSum !== undefined && currentSum > previousSum) {
       increases += 1;
     }
