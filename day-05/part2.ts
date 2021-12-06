c508b86199a272aec7c8739c810d6e0abe50d70c
mattbalmer <signup@mattbalmer.com>
2021-12-05 19:21:21 -0800

    Clean day 5 part 2

--- day-05/part2.ts
+++ day-05/part2.ts
@@ -5,36 +5,26 @@ export const sumOverlap = (input: string[]): number => {
 
   for(let i = 0; i < input.length; i++) {
     const line = parseLine(input[i]);
-    const vertical = line[0][0] === line[1][0];
-    const horizontal = line[0][1] === line[1][1];
 
-    if (vertical || horizontal) {
-      const c = horizontal ? 0 : 1;
-      const start = Math.min(line[0][c], line[1][c])
-      const end = Math.max(line[0][c], line[1][c]);
-      const locked = line[0][c ? 0 : 1];
-      for(let scaling = start; scaling <= end; scaling++) {
-        const x = horizontal ? scaling : locked;
-        const y = vertical ? scaling : locked;
-        const key = `${x},${y}`;
-        if(!counts[key]) {
-          counts[key] = 0;
-        }
-        counts[key] += 1;
+    const xDir = Math.sign(line[1][0] - line[0][0]);
+    const yDir = Math.sign(line[1][1] - line[0][1]);
+
+    const vertical = !xDir;
+
+    const length = Math.abs(
+      vertical
+        ? line[1][1] - line[0][1]
+        : line[1][0] - line[0][0]
+    );
+
+    for(let p = 0; p <= length; p++) {
+      const x = line[0][0] + (p * xDir);
+      const y = line[0][1] + (p * yDir);
+      const key = `${x},${y}`;
+      if(!counts[key]) {
+        counts[key] = 0;
       }
-    } else {
-      const length = Math.abs(line[1][0] - line[0][0]);
-      const xDir = Math.sign(line[1][0] - line[0][0]);
-      const yDir = Math.sign(line[1][1] - line[0][1]);
-      for(let p = 0; p <= length; p++) {
-        const x = line[0][0] + (p * xDir);
-        const y = line[0][1] + (p * yDir);
-        const key = `${x},${y}`;
-        if(!counts[key]) {
-          counts[key] = 0;
-        }
-        counts[key] += 1;
-      }
+      counts[key] += 1;
     }
   }
 
