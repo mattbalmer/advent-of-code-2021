c94d5da449b080b781abb14557a1c490bec2726c
mattbalmer <signup@mattbalmer.com>
2021-12-11 22:06:24 -0800

    data template

---
+++ day-template/part1.ts
@@ -0,0 +1,10 @@
+export const main = (): number => {
+  return 4;
+}
+
+export const execute = (inputs: string[]): number =>
+  main(
+    // inputs[0]
+    //   .split(',')
+    //   .map(_ => parseInt(_, 10)),
+  )
