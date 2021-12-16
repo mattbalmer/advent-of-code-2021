6837d82e79a735670bc51f6b6feb4c01aea627f1
mattbalmer <signup@mattbalmer.com>
2021-12-15 19:14:41 -0800

    Use a lowest risk map

--- day-15/part1.ts
+++ day-15/part1.ts
@@ -1,24 +1,12 @@
 type Coordinate = [number, number];
 type Grid = number[][];
-type PartialPath = {
-  score: number,
-  risk: number,
-  history: string[],
-}
 
-const AVERAGE_RISK_PER_CELL = 4.5;
 const ADJACENT_DELTAS = [
   [0,1],
   [1,0],
-  [-1,0],
-  [0,-1],
-]
+];
 
 const valueAt = (grid: Grid, coordinate: Coordinate): number => grid[coordinate[1]][coordinate[0]];
-const distanceBetween = (a: Coordinate, b: Coordinate): number =>
-  Math.abs(b[0] - a[0]) + Math.abs(b[1] - a[1]);
-const riskBetween = (a: Coordinate, b: Coordinate): number =>
-  (distanceBetween(a, b) - 1) * AVERAGE_RISK_PER_CELL;
 const coordsMatch = (a: Coordinate, b: Coordinate): boolean => a[0] === b[0] && a[1] === b[1];
 const isCoordValid = (grid: Grid, coord: Coordinate): boolean =>
   coord[0] >= 0 && coord[0] < grid.length && coord[1] >= 0 && coord[1] < grid.length;
@@ -28,43 +16,34 @@ const strToCoord = (str: string): Coordinate => str.split(',').map(_ => parseInt
 export const main = (grid: Grid): number => {
   const start: Coordinate = [0,0];
   const end: Coordinate = [grid.length - 1, grid.length - 1];
+  const LOWEST_RISK_TO: Record<string, number> = {
+    [coordToStr(start)]: 0
+  };
 
-  const paths: PartialPath[] = [{
-    score: riskBetween(start, end),
-    risk: 0,
-    history: [coordToStr(start)]
-  }];
+  const coordinates: [number, Coordinate][] = [[0, start]];
 
-  while(paths.length > 0) {
-    const { score, risk, history }: PartialPath = paths.shift();
-    const coord: Coordinate = strToCoord(history[history.length - 1]);
-    console.log(score, risk, history);
-
-    if (coordsMatch(coord, end)) {
-      return risk;
-    }
+  while(coordinates.length > 0) {
+    const [risk, coord] = coordinates.shift();
+    const coordString = coordToStr(coord);
 
     ADJACENT_DELTAS
       .map(delta =>
         [coord[0] + delta[0], coord[1] + delta[1]] as Coordinate
       )
       .filter(c =>
-        isCoordValid(grid, c) &&
-        !history.includes(coordToStr(c))
+        isCoordValid(grid, c)
       )
-      .map(c =>
-        [risk + valueAt(grid, c)/* + riskBetween(c, end)*/, valueAt(grid, c), c] as [number, number, Coordinate]
-      )
-      .sort((a, b) => a[0] - b[0])
-      .forEach(([nScore, nRisk, nCoord]) => {
-        const i = paths.findIndex(partial => partial.score > nScore);
-        paths.splice(i >= 0 ? i : paths.length, 0, {
-          score: nScore,
-          risk: risk + nRisk,
-          history: [...history, coordToStr(nCoord)],
-        });
+      .forEach(c => {
+        const r = valueAt(grid, c);
+        const key = coordToStr(c);
+        LOWEST_RISK_TO[key] = Math.min(risk + r, LOWEST_RISK_TO[key] || Number.MAX_SAFE_INTEGER);
+        coordinates.push([risk + r, c]);
       });
   }
+
+  console.log(LOWEST_RISK_TO);
+
+  return LOWEST_RISK_TO[coordToStr(end)];
 }
 
 export const execute = (inputs: string[]): number =>
